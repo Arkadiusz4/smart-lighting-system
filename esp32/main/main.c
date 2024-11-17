8d9530f2c12aa133f4527e760ea69822a00fe9b4
Arkadiusz4 <arekmika4@gmail.com>
2024-11-17 19:12:29 +0100

    Add mqtt broker

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -3,6 +3,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_tls.h"
+#include "mqtt_broker.h"
 
 void http_request_task(void *pvParameters) {
     esp_tls_init_global_ca_store();
@@ -19,4 +20,8 @@ void app_main(void) {
     }
 
     xTaskCreate(&http_request_task, "http_request_task", 8192, NULL, 5, NULL);
+
+    mqtt_app_start();
+
+    mqtt_publish("user123/esp32_1/motion", "Motion detected");
 }

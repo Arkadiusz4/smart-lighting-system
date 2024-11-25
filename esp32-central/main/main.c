3f4e1f1381a64b1eea557d0b06f929369ea49bd9
Arkadiusz4 <arekmika4@gmail.com>
2024-11-25 13:11:46 +0100

    Update logic of init ble and wifi

--- esp32-central/main/main.c
+++ esp32-central/main/main.c
@@ -7,31 +7,53 @@
 #include "mqtt_broker.h"
 #include "esp_log.h"
 
-void http_request_task(void *pvParameters) {
+void http_request_task(void *pvParameters)
+{
     esp_tls_init_global_ca_store();
     http_client_get("https://example.com");
     esp_tls_free_global_ca_store();
     vTaskDelete(NULL);
 }
 
-void app_main(void) {
+void ble_task(void *pvParameters)
+{
     esp_err_t ret;
 
-    wifi_manager_init();
-
-    while (!wifi_manager_is_connected()) {
-        ESP_LOGI("MAIN", "Czekam na połączenie Wi-Fi...");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-
-    mqtt_app_start();
-
     ret = ble_central_init();
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE("MAIN", "Failed to initialize BLE Central: %s", esp_err_to_name(ret));
         return;
     }
     ESP_LOGI("MAIN", "BLE Central initialized successfully.");
 
-    xTaskCreate(&http_request_task, "http_request_task", 8192, NULL, 5, NULL);
+    vTaskDelete(NULL);
 }
+
+void wifi_task(void *pvParameters)
+{
+    wifi_manager_init();
+
+    while (!wifi_manager_is_connected())
+    {
+        ESP_LOGI("MAIN", "Czekam na połączenie Wi-Fi...");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    ESP_LOGI("WIFI_TASK", "Wi-Fi connected");
+
+    mqtt_app_start();
+
+        xTaskCreate(&http_request_task, "http_request_task", 8192, NULL, 5, NULL);
+
+
+    vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    xTaskCreate(&ble_task, "ble_task", 8192, NULL, 5, NULL);
+
+    xTaskCreate(&wifi_task, "wifi_task", 8192, NULL, 5, NULL);
+}
+

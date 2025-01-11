c9821a168013818e9ef421b59e731f666cf50cc8
Arkadiusz4 <arekmika4@gmail.com>
2025-01-11 11:43:39 +0100

    Create led_switch.dart

---
+++ mobile-app/lib/screens/home_screen/led_switch.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_app/blocs/devices/devices_bloc.dart';
+import 'package:mobile_app/blocs/devices/devices_event.dart';
+import 'package:mobile_app/models/device.dart';
+import 'package:mobile_app/styles/color.dart';
+
+class LedSwitch extends StatefulWidget {
+  final Device device;
+  final String userId;
+
+  const LedSwitch({Key? key, required this.device, required this.userId}) : super(key: key);
+
+  @override
+  _LedSwitchState createState() => _LedSwitchState();
+}
+
+class _LedSwitchState extends State<LedSwitch> {
+  late bool currentValue;
+
+  @override
+  void initState() {
+    super.initState();
+    currentValue = widget.device.status == 'on';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      title: const Text('Włącz/wyłącz LED', style: TextStyle(color: textColor)),
+      value: currentValue,
+      onChanged: (bool value) {
+        setState(() {
+          currentValue = value;
+        });
+        print("Toggle LED clicked, new value: $value");
+
+        context.read<DevicesBloc>().add(
+              ToggleLed(widget.device.deviceId, value),
+            );
+      },
+    );
+  }
+}

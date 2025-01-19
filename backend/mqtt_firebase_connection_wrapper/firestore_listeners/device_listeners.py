28284e7906af812f6c49d23f8aa1798a21907a8f
Arkadiusz4 <arekmika4@gmail.com>
2025-01-19 20:13:09 +0100

    Update mqtt sub and pub

--- backend/mqtt_firebase_connection_wrapper/firestore_listeners/device_listeners.py
+++ backend/mqtt_firebase_connection_wrapper/firestore_listeners/device_listeners.py
@@ -3,12 +3,20 @@ import json
 
 def publish_device_update(mqtt_client, board_id, device_id, device_data):
     topic = f"boards/{board_id}/devices/{device_id}"
-    payload = json.dumps({
+
+    payload_data = {
         "status": device_data.get("status", "unknown"),
         "deviceId": device_id,
         "port": device_data.get("port", "unknown"),
         "type": device_data.get("type", "unknown"),
-    })
+    }
+
+    # Jeśli typ to "Sensor ruchu", dodaj dodatkowe pola
+    if device_data.get("type") == "Sensor ruchu":
+        payload_data["pir_cooldown_time"] = device_data.get("pir_cooldown_time", "0")
+        payload_data["led_on_duration"] = device_data.get("led_on_duration", "0")
+
+    payload = json.dumps(payload_data)
     print(f"{mqtt_client}")
     mqtt_client.publish(topic, payload)
     print(f"Published to {topic} => {payload}")
@@ -28,4 +36,14 @@ def on_devices_snapshot(board_id, mqtt_client, col_snapshot, changes, read_time)
             publish_device_update(mqtt_client, board_id, device_id, device_data)
 
         elif change.type.name == "REMOVED":
-            print(f"Device removed: {change.document.id}")
+            device_id = change.document.id
+            print(f"Device removed: {device_id}")
+
+            # Publikujemy JSON z "status": "removed"
+            topic = f"boards/{board_id}/devices/{device_id}"
+            payload = json.dumps({
+                "deviceId": device_id,
+                "status": "removed"
+            })
+            mqtt_client.publish(topic, payload)
+            print(f"Published 'removed' for {device_id}: {payload}")
--- backend/mqtt_firebase_connection_wrapper/mosquitto_utils/FirebaseMQTTListener.py
+++ backend/mqtt_firebase_connection_wrapper/mosquitto_utils/FirebaseMQTTListener.py
@@ -1,32 +1,21 @@
-import json
 import subprocess
 import time
+import json
+import threading
+from datetime import datetime
 
 import paho.mqtt.client as mqtt
+from firebase_admin import firestore
 
 BROKER_ADDRESS = "192.168.0.145"
 BROKER_PORT = 2137
-
-
-def create_on_message_callback(user_id):
-    def on_message(client, userdata, msg):
-        try:
-            payload = msg.payload.decode()
-            try:
-                data = json.loads(payload)
-                print(f"Client {user_id} received JSON on {msg.topic}: {data}")
-            except json.JSONDecodeError:
-                print(f"Client {user_id} received message on {msg.topic}: {payload}")
-        except Exception as e:
-            print(f"Client {user_id}: Error processing message: {e}")
-
-    return on_message
 
 
 def create_on_connect_callback(user_id, boardId):
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print(f"[INFO] Client {user_id} connected to MQTT broker on board {boardId}!")
+            # Subskrybuj wszystkie tematy dla danego boardId
             client.subscribe(f"boards/{boardId}/#")
         else:
             print(f"[ERROR] Client {user_id} connection failed with code: {rc}")
@@ -36,32 +25,71 @@ def create_on_connect_callback(user_id, boardId):
     return on_connect
 
 
-def restart_mosquitto():
-    try:
-        print("[INFO] Restarting Mosquitto service...")
-        result = subprocess.run(
-            ["brew", "services", "restart", "mosquitto"],
-            capture_output=True,
-            text=True
-        )
-        if result.returncode == 0:
-            print("[INFO] Mosquitto service restarted successfully.")
-        else:
-            print(f"[ERROR] Failed to restart Mosquitto: {result.stderr}")
-    except Exception as e:
-        print(f"[ERROR] Exception occurred while restarting Mosquitto: {e}")
-
-
 class FirebaseMQTTListener:
     def __init__(self, db, mqtt_user_manager, acl_manager):
         self.db = db
         self.mqtt_user_manager = mqtt_user_manager
         self.acl_manager = acl_manager
         self.user_clients = {}
+        self.last_heartbeat = {}
+        self.board_user_map = {}
+        self.lost_boards = set()
 
-    def start_listening(self, collection_path):
-        collection_ref = self.db.collection(collection_path)
-        collection_ref.on_snapshot(self.on_mqtt_clients_snapshot)
+    def create_on_message_callback(self):
+        def on_message(client, userdata, msg):
+            try:
+                payload = msg.payload.decode()
+                try:
+                    data = json.loads(payload)
+                except json.JSONDecodeError:
+                    data = None
+
+                print(f"Received message on {msg.topic}: {payload}")
+
+                # Obsługa heartbeat
+                if msg.topic.endswith("/heartbeat"):
+                    board_id = msg.topic.split('/')[1]
+                    self.last_heartbeat[board_id] = datetime.utcnow()
+                    print(f"Heartbeat received from board {board_id} at {self.last_heartbeat[board_id]}")
+                    return
+
+                # Dla wiadomości ruchu lub sieci
+                log_entry = None
+                if msg.topic.endswith("/motion"):
+                    log_entry = {
+                        "timestamp": firestore.SERVER_TIMESTAMP,
+                        "message": "Wykryto ruch!",
+                        "eventType": "motion_detected",
+                        "device": data.get("deviceId") if data else None,
+                        "severity": "warning",
+                        "boardId": msg.topic.split('/')[1]
+                    }
+                elif msg.topic.endswith("/network") and payload == "connected":
+                    log_entry = {
+                        "timestamp": firestore.SERVER_TIMESTAMP,
+                        "message": "Połączono z Wi-Fi!",
+                        "eventType": "wifi_connected",
+                        "device": "Network",
+                        "severity": "info",
+                        "boardId": msg.topic.split('/')[1]
+                    }
+                else:
+                    print(f"Unhandled message on {msg.topic}: {payload}")
+                    return
+
+                board_id = msg.topic.split('/')[1]
+                user_id = self.board_user_map.get(board_id)
+
+                if user_id:
+                    self.db.collection("users").document(user_id) \
+                        .collection("logs").add(log_entry)
+                else:
+                    self.db.collection("logs").add(log_entry)
+                print("Log entry added to Firestore.")
+            except Exception as e:
+                print(f"Error processing message: {e}")
+
+        return on_message
 
     def on_mqtt_clients_snapshot(self, col_snapshot, changes, read_time):
         for change in changes:
@@ -71,6 +99,9 @@ class FirebaseMQTTListener:
             mqtt_password = data.get("mqtt_password")
             board_id = data.get("boardId")
             topic = f"boards/{board_id}/#"
+
+            if board_id and user_id:
+                self.board_user_map[board_id] = user_id
 
             if change.type.name in ("ADDED", "MODIFIED"):
                 print(f"Aktualizacja lub dodanie użytkownika: {user_id}")
@@ -87,7 +118,7 @@ class FirebaseMQTTListener:
                         client = mqtt.Client(client_id=f"client_{user_id}")
                         client.username_pw_set(username=user_id, password=mqtt_password)
                         client.on_connect = create_on_connect_callback(user_id, board_id)
-                        client.on_message = create_on_message_callback(user_id)
+                        client.on_message = self.create_on_message_callback()
 
                         max_retries = 5
                         for attempt in range(max_retries):
@@ -111,7 +142,6 @@ class FirebaseMQTTListener:
                         except Exception as e:
                             print(f"Błąd przy aktualizacji klienta MQTT dla {user_id}: {e}")
 
-                    # self.user_clients[user_id].subscribe(f"some/topic/for/{user_id}")
             elif change.type.name == "REMOVED":
                 print(f"Dokument usunięty: {doc.id}")
                 if user_id in self.user_clients:
@@ -120,3 +150,71 @@ class FirebaseMQTTListener:
                     client.disconnect()
                     del self.user_clients[user_id]
                     print(f"MQTT client for user {user_id} stopped and removed.")
+
+    def start_listening(self, collection_path):
+        collection_ref = self.db.collection(collection_path)
+        collection_ref.on_snapshot(self.on_mqtt_clients_snapshot)
+
+    def monitor_heartbeats(self, check_interval=60, timeout=120):
+        while True:
+            now = datetime.utcnow()
+            # Sprawdzenie brakujących heartbeatów
+            for board_id, last_time in self.last_heartbeat.items():
+                if (now - last_time).total_seconds() > timeout and board_id not in self.lost_boards:
+                    print(f"No heartbeat from board {board_id} since {last_time}, logging error.")
+                    user_id = self.board_user_map.get(board_id)
+                    log_entry = {
+                        "timestamp": firestore.SERVER_TIMESTAMP,
+                        "message": f"BRAK POŁĄCZENIA Z {board_id}",
+                        "eventType": "connection_lost",
+                        "device": 'Network',
+                        "severity": "critical",
+                        "boardId": board_id
+                    }
+                    if user_id:
+                        self.db.collection("users").document(user_id) \
+                            .collection("logs").add(log_entry)
+                    else:
+                        self.db.collection("logs").add(log_entry)
+                    self.lost_boards.add(board_id)
+
+            for board_id in list(self.lost_boards):
+                if board_id in self.last_heartbeat:
+                    time_since_last = (now - self.last_heartbeat[board_id]).total_seconds()
+                    if time_since_last <= timeout:
+                        print(f"Heartbeat restored for board {board_id}, removing from lost_boards.")
+                        self.lost_boards.remove(board_id)
+
+                        user_id = self.board_user_map.get(board_id)
+                        log_entry = {
+                            "timestamp": firestore.SERVER_TIMESTAMP,
+                            "message": f"POŁĄCZENIE PRZYWRÓCONE Z {board_id}",
+                            "eventType": "connection_restored",
+                            "device": 'Network',
+                            "severity": "info",
+                            "boardId": board_id
+                        }
+                        if user_id:
+                            self.db.collection("users").document(user_id) \
+                                .collection("logs").add(log_entry)
+                        else:
+                            self.db.collection("logs").add(log_entry)
+                        print(f"Connection restored log entry added for board {board_id}.")
+
+            time.sleep(check_interval)
+
+
+def restart_mosquitto():
+    try:
+        print("[INFO] Restarting Mosquitto service...")
+        result = subprocess.run(
+            ["brew", "services", "restart", "mosquitto"],
+            capture_output=True,
+            text=True
+        )
+        if result.returncode == 0:
+            print("[INFO] Mosquitto service restarted successfully.")
+        else:
+            print(f"[ERROR] Failed to restart Mosquitto: {result.stderr}")
+    except Exception as e:
+        print(f"[ERROR] Exception occurred while restarting Mosquitto: {e}")

ca690b825567f97832f0ad002c6e7794ff2bb894
Przemyslaw Orlikowski <orprzemek@gmail.com>
2025-01-20 00:56:27 +0100

    refactor

--- mobile-app/lib/models/motion_sensor.dart
+++ mobile-app/lib/models/motion_sensor.dart
@@ -1,31 +1,31 @@
 import 'device.dart';
 
 class MotionSensor extends Device {
-final int ledOnDuration;
-final int pirCooldownTime;
+  final int ledOnDuration;
+  final int pirCooldownTime;
 
-MotionSensor({
-required super.deviceId,
-required super.name,
-required super.type,
-required super.port,
-required super.boardId,
-super.status,
-required this.ledOnDuration,
-required this.pirCooldownTime,
-});
+  MotionSensor({
+    required super.deviceId,
+    required super.name,
+    required super.type,
+    required super.port,
+    required super.boardId,
+    super.status,
+    required this.ledOnDuration,
+    required this.pirCooldownTime,
+  });
 
-factory MotionSensor.fromDevice(Device device, {required int ledOnDuration, required int pirCooldownTime}) {
-
-  return MotionSensor(
-deviceId: device.deviceId,
-name: device.name,
-type: device.type,
-port: device.port,
-boardId: device.boardId,
-status: device.status,
-ledOnDuration: ledOnDuration,
-pirCooldownTime: pirCooldownTime,
-);
-}
+  factory MotionSensor.fromDevice(Device device,
+      {required int ledOnDuration, required int pirCooldownTime}) {
+    return MotionSensor(
+      deviceId: device.deviceId,
+      name: device.name,
+      type: device.type,
+      port: device.port,
+      boardId: device.boardId,
+      status: device.status,
+      ledOnDuration: ledOnDuration,
+      pirCooldownTime: pirCooldownTime,
+    );
+  }
 }
